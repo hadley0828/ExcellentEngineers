c6049087f26098c834ee0c67c5aa373195009f6c
151250197 <151250197@smail.nju.edu.cn>
2017-07-20 10:37:03 +0800

    修改输出

--- MachineLearning/src/main/resources/static/execute.js
+++ MachineLearning/src/main/resources/static/execute.js
@@ -6,11 +6,11 @@ function translateexecute() {
     var option = $("#s option:selected")
     if(option.text() == "英文转中文") {
         $.post('/etoc/translate',{input: text}, function (data) {
-
+            document.getElementById('output').value = data
         })
     }else{
         $.post('/ctoe/translate',{input: text}, function (data) {
-            
+            document.getElementById('output').value = data
         })
     }
 }
