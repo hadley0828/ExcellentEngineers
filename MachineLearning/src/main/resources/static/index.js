d5c5db6b6c235aff70f84d855c3c336f1fe24532
151070025 <151070025@smail.nju.edu.cn>
2017-07-26 10:13:35 +0800

    更新界面

--- MachineLearning/src/main/resources/static/index.js
+++ MachineLearning/src/main/resources/static/index.js
@@ -1,10 +1,28 @@
 /**
  * Created by fourtwo42 on 2017/7/14.
- **/
+ */
 'use strict';
 (function(){
-    var input = new Vue({
-        el: "#content",
+    var func = new Vue({
+        el:"#function",
+        methods:{
+            transHover: function(){
+                document.getElementById('trans').src = "img/translate_black.png";
+            },
+            transLeave: function(){
+                document.getElementById('trans').src = "img/translate_white.png";
+            },
+            evalHover: function(){
+                document.getElementById('eval').src = "img/star_black.png";
+            },
+            evalLeave: function(){
+                document.getElementById('eval').src = "img/star_white.png";
+            }
+        }
+    });
+
+    var transPage = new Vue({
+        el: "#trans-page",
         data: {
             wordNum: 0,
             selectedLanguage: 'auto',
@@ -16,22 +34,22 @@
         },
         methods: {
             clearNotice: function () {
-                document.getElementById('clear-notice').style.visibility = "visible";
+                document.getElementById('trans-clear-notice').style.visibility = "visible";
             },
             removeClearNotice: function () {
-                document.getElementById('clear-notice').style.visibility = "hidden";
+                document.getElementById('trans-clear-notice').style.visibility = "hidden";
             },
             clearInput: function () {
-                document.getElementById('input').value = "";
+                document.getElementById('trans-input').value = "";
                 this.wordNum = 0;
             },
             countWordNum: function () {
-                this.wordNum = document.getElementById('input').value.length;
+                this.wordNum = document.getElementById('trans-input').value.length;
                 if(this.wordNum == 0){
                     this.selectedLanguage = 'auto';
                 }
                 else {
-                        var content = document.getElementById('input').value.substr(0, 1);
+                        var content = document.getElementById('trans-input').value.substr(0, 1);
                         var re = new RegExp("[\\u4E00-\\u9FFF]+", "g");
                         if (re.test(content)) {
                             this.selectedLanguage = '2';
@@ -42,5 +60,27 @@
                 }
             }
         },
-    })
+    });
+
+    var evalPage = new Vue({
+        el: "#eval-page",
+        data: {
+            wordNum: 0,
+        },
+        methods: {
+            clearNotice: function () {
+                document.getElementById('eval-clear-notice').style.visibility = "visible";
+            },
+            removeClearNotice: function () {
+                document.getElementById('eval-clear-notice').style.visibility = "hidden";
+            },
+            clearInput: function () {
+                document.getElementById('eval-input').value = "";
+                this.wordNum = 0;
+            },
+            countWordNum: function () {
+                this.wordNum = document.getElementById('eval-input').value.length;
+            }
+        },
+    });
 })();
